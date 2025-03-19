d60343fef3becaf16cbab26f426d822015fbb53e
Niek Ilmer <niek.ilmer.aj@renesas.com>
2025-03-19 15:24:02 +0100

    soc: renesas: ra: Add RA2A2 SoC support Add Support for Renesas RA2A2 SoC.
    Signed-off-by: Niek Ilmer <niek.ilmer.aj@renesas.com>

---
+++ soc/renesas/ra/ra2a2/soc.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 Renesas Electronics Corporation and/or its affiliates
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the Renesas RA2A1 family MCU
+ */
+
+#ifndef ZEPHYR_SOC_RENESAS_RA2A1_SOC_H_
+#define ZEPHYR_SOC_RENESAS_RA2A1_SOC_H_
+
+#include <bsp_api.h>
+
+#endif /* ZEPHYR_SOC_RENESAS_RA2A1_SOC_H_ */
